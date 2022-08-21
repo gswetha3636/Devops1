36e628c6c2c263c2e30493e09147c04d1b0c3774
swetha <gswetha3636@gmail.com>
2022-08-21 18:40:22 -0400

    first vpc and subnet creation code

---
+++ Create_vpc.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "vpc1" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "first_vpc1"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.vpc1.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet _1 "
+  }
+}
--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ provider "aws" {
   secret_key = "giR7UbBSpmh9t/xdBLFR5I6VywCwqcdXg1Ruw/v1"
 }
 
-resource "aws_instance" "swetha-server1" {
-  ami = "ami-051dfed8f67f095f5"
-  instance_type = "t2.micro"
+#resource "aws_instance" "swetha-server1" {
+ # ami = "ami-051dfed8f67f095f5"
+  #instance_type = "t2.micro"
 
-  tags = {
-  Name = "HelloAws 1st instance"
- }
-}
+  #tags = {
+ # Name = "HelloAws 1st instance"
+ #}
+#}
