06deb44d0439fa5b486f337fb222c86c5994d12f
Swetha <gswetha3636@gmail.com>
2022-08-27 15:34:09 -0400

    second vpc ,subnets

--- vpc2.tf
+++ vpc2.tf
@@ -7,11 +7,14 @@ resource "aws_vpc" "vpc2" {
   }
 }
 
-resource "aws_subnet" "subnet1" {
+resource "aws_subnet" "sub1" {
   vpc_id     = aws_vpc.vpc2.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "subnet _1   "
+    Name = " sub1   "
   }
 }
+
+
+
